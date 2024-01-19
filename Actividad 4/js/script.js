819130c07c168c228c81a997c6f66d45b9ebde8c
dplacor1704 <151845139+dplacor1704@users.noreply.github.com>
2024-01-19 11:52:43 +0100

    Add files via upload

---
+++ Actividad 4/js/script.js
@@ -0,0 +1,12 @@
+function hola() {
+  let a, b, c, d, e, f, g;
+  a= parseInt(prompt("Introduce el primer número:"));
+  b= parseInt(prompt("Introduce el segundo número"));
+  c= a+b;
+  d= a-b;
+  e= a*b;
+  f= a/b;
+  g= a%b;
+
+  alert ("El primer número es: " + a + "\nEl segndo número es: " + b + "\nLa suma es: " + c + "\nLa resta es: " + d + "\nEl producto es: " + e + "\nLa división es: " + f + "\nEl resto es: " + g);
+  }
